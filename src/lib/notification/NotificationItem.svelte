e04bd315e6771fdf50c848584ee088fdbb3af920
Aravind-SL <aravind9180@gmail.com>
2024-02-22 21:34:49 +0530

    Slight modification on buttons and added AU_logo

--- src/lib/notification/NotificationItem.svelte
+++ src/lib/notification/NotificationItem.svelte
@@ -7,7 +7,7 @@
 </script>
 
 
-<div class="min-w-0.5 bg-slate-400 p-2 md:p-4 rounded-md">
+<div class="min-w-0.5 bg-cyan-500 shadow-lg shadow-cyan-500/50 p-2 md:p-4 rounded-md">
     <h3 class="md:text-lg mb-2 md:mb-4">{heading}</h3>
     <p class="text-xs md:text-md">{content}</p>
 </div>
