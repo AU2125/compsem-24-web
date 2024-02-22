748dfeae3bae6aeda868139cd5047ceb816ef6a5
amirth300324@gmail.com <amirth300324@gmail.com>
2024-02-22 14:09:57 +0530

    Updated Home page UI into something looking good

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,14 +3,15 @@
 
 </script>
 
-<header class="bg-transparent text-2xl px-8 py-6 z-10 flex justify-between w-screen">
+<header class="px-8 py-6 z-10 absolute top-0 left-0 flex justify-center md:justify-between w-screen">
     <Link to="/compsem-24-web" >
-        <h3 class="text-gray-200">Compsem <span class="text-gray-800">'24</span></h3>
+        <h3 class="md:text-2xl text-gray-200">Compsem<span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-300 to-red-400">'24</span></h3>
     </Link>
 
-    <nav class="flex text-sm justify-evenly w-1/3 text-gray-200">
-        <Link to="compsem-24-web/" class="p-2 rounded-md hover:bg-gray-500 hover:text-gray-100">Home</Link>
-        <Link to="compsem-24-web/events/technical" class="p-2 rounded-md hover:bg-gray-500 hover:text-gray-100">Technical Events</Link>
-        <Link to="compsem-24-web/events/nontechnical" class="p-2 rounded-md hover:bg-gray-500 hover:text-gray-100">Non Technical Events</Link>
+    <nav class="md:flex hidden text-sm justify-evenly w-1/3 text-gray-200">
+        <Link to="compsem-24-web/" class="p-2 rounded-md hover:text-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">Home</Link>
+        <Link to="compsem-24-web/events/technical" class="p-2 hover:text-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">Technical Events</Link>
+        <Link to="compsem-24-web/events/nontechnical" class="p-2 hover:text-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">Non Technical Events</Link>
+        <a href="/compsem-24-web/#contacts" class="p-2 hover:text-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">Contact Us</a>
     </nav>
 </header>
