a72078bd9927fdfa16ad3dd96592ed3ee537db19
amirth300324@gmail.com <amirth300324@gmail.com>
2024-02-22 09:30:03 +0530

    Added Some links to header

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,13 @@
 <script>
-    import {Router, Link, Route} from 'svelte-routing';
+    import {Router,  Route} from 'svelte-routing';
     import HomePage from './pages/Home.svelte';
     import EventsPage from './pages/EventsPage.svelte';
-
+    import Header from './lib/Header.svelte';
     export let url = "";
 </script>
 
 <Router url={url}>
-    <header class="bg-transparent text-2xl px-8 py-6 z-10 flex w-screen">
-        <Link to="/compsem-24-web" >
-            <h3 class="text-gray-200">Compsem <span class="text-gray-800">'24</span></h3>
-        </Link>
-
-
-    </header>
+    <Header />
     <Route path="/compsem-24-web/"><HomePage/></Route>
     <Route path="/compsem-24-web/events/:evType" component={EventsPage}></Route>
 </Router>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+    import  {Link} from 'svelte-routing';
+
+</script>
+
+<header class="bg-transparent text-2xl px-8 py-6 z-10 flex justify-between w-screen">
+    <Link to="/compsem-24-web" >
+        <h3 class="text-gray-200">Compsem <span class="text-gray-800">'24</span></h3>
+    </Link>
+
+    <nav class="flex text-sm justify-evenly w-1/3 text-gray-200">
+        <Link to="compsem-24-web/" class="p-2 rounded-md hover:bg-gray-500 hover:text-gray-100">Home</Link>
+        <Link to="compsem-24-web/events/technical" class="p-2 rounded-md hover:bg-gray-500 hover:text-gray-100">Technical Events</Link>
+        <Link to="compsem-24-web/events/nontechnical" class="p-2 rounded-md hover:bg-gray-500 hover:text-gray-100">Non Technical Events</Link>
+    </nav>
+</header>
