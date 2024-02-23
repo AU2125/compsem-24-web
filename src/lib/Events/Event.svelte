a679eb5eb577999a6f4d0f7819fa3d620b4dd452
amirth300324@gmail.com <amirth300324@gmail.com>
2024-02-23 11:12:54 +0530

    Fixed Mobile responsive Issure in Events Pages

--- src/lib/Events/Event.svelte
+++ src/lib/Events/Event.svelte
@@ -4,7 +4,7 @@
     export let events: IEvent[];
 </script>
 
-<div class="flex flex-wrap justify-center gap-8 px-32 mt-12">
+<div class="flex items-center flex-wrap justify-center gap-8 px-32 mt-12">
     {#each events as event}
     <EventItem to={event.to} heading={event.heading} date={event.scheduledDate} oneliner={event.oneliner} />
     {/each}
