efde4537c679e1ef2191ef3be8d8e0d111cfcc45
Aravind-SL <aravind9180@gmail.com>
2024-02-24 13:00:48 +0530

    modfied footer structure

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,9 +3,9 @@
         <a class="justify-center md:mb-2 md:text-3xl items-center flex" href="https://annamalaiuniversity.ac.in/index.php">
             <p>Annamalai</p> <img class="px-1 w-6 md:w-12" src="AU_logo.png" alt="Annamalai University Logo"><p>University</p>
         </a>
-        <p class="text-[6px] md:text-xs text-gray-300 normal-case">(Accredited with 'A<sup>+</sup>' Grade by NAAC)</p>
-        <p class="text-[8px] md:text-sm">Faculty of Engineering and Technology</p>
         <p class="text-[8px] md:text-sm">Department of Computer Science and Engineering</p>
+        <p class="text-[8px] md:text-sm">Faculty of Engineering and Technology</p>
+        <p class="text-[6px] md:text-xs text-gray-300 normal-case">(Accredited with 'A<sup>+</sup>' Grade by NAAC)</p>
     </div>
     <div class="w-full flex justify-between">
         <p class="bg-clip-text text-[6px] md:text-sm text-transparent bg-gradient-to-r from-pink-300 to-red-400">All Rights Reserved &copy; Department of CSE.</p>
