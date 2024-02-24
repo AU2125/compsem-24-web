957080c21cbb886c51c99c1fb737682528a460d5
Aravind-SL <aravind9180@gmail.com>
2024-02-24 11:20:48 +0530

    Font modification

--- src/lib/Events/EventItem.svelte
+++ src/lib/Events/EventItem.svelte
@@ -9,7 +9,7 @@
     export let date: Date;
     export let to:string;
 </script>
-<Link class="border-rose-400 border-2 group text-sky-400 h-48 py-10 px-4 w-64 md:w-96 flex justify-center rounded-md items-center flex-col cursor-pointer hover:bg-slate-500/20" to="/event/{to}">
+<Link class="border-rose-400 border-2 group text-gray-200 h-48 py-10 px-4 w-64 md:w-96 flex justify-center rounded-md items-center flex-col cursor-pointer hover:bg-slate-500/20" to="/event/{to}">
     <div class="font-anta text-sm md:text-xl w-full text-wrap text-center group-hover:text-2xl text-wrap uppercase leading-tight ">{heading}</div>
     <span class="text-center">
 
