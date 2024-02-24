cc93fe00f94e3ba75f6039b6a01c69498136b7ef
amirth300324@gmail.com <amirth300324@gmail.com>
2024-02-24 07:05:56 +0530

    Fixed Routing Issues

--- src/lib/Events/EventItem.svelte
+++ src/lib/Events/EventItem.svelte
@@ -9,10 +9,10 @@
     export let date: Date;
     export let to:string;
 </script>
-<Link class="border-rose-400 border-2 group text-gray-200 h-32 py-10 px-4 w-64 md:w-96 flex justify-center rounded-md items-center flex-col cursor-pointer hover:bg-slate-500/20" to="compsem-24-web/event/{to}">
+<Link class="border-rose-400 border-2 group text-blue-400 h-48 py-10 px-4 w-64 md:w-96 flex justify-center rounded-md items-center flex-col cursor-pointer hover:bg-slate-500/20" to="/event/{to}">
     <div class="font-anta text-sm md:text-xl w-full text-wrap text-center group-hover:text-2xl text-wrap uppercase leading-tight ">{heading}</div>
     <span class="text-center">
-        
+
         <div class="font-kdam text-xs group-hover:text-lg md:text-md">{calcDate(date)}</div>
     </span>
 </Link>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <header class="px-8 py-6 z-10 absolute top-0 left-0 flex flex-col md:flex-row items-center justify-center md:justify-between w-screen">
-    <Link to="/compsem-24-web" >
-        <h3 class="font-anta text-3xl text-gray-200">Compsem<span class="font-anta bg-clip-text text-transparent bg-gradient-to-r from-pink-300 to-red-400">'24</span></h3>
+    <Link to="/" >
+        <h3 class="text-3xl text-gray-200">Compsem<span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-300 to-red-400">'24</span></h3>
     </Link>
 
    <button on:click={
@@ -17,9 +17,9 @@
    </button>
 
     <nav class="flex bg-gray-800/40 md:bg-transparent {mdScreen ? '' : menuToggle ? 'border-2 border-rose-400 animate-clip-text-from-above flex-col text-center rounded-md items-center backdrop-blur-sm' : 'hidden'} text-sm justify-evenly md:gap-4  text-gray-200">
-        <Link on:click= {() => menuToggle = false} to="compsem-24-web/" class="p-2 transition-transform duration-75 ease-in-out hover:scale-125 rounded-md hover:text-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">Home</Link>
-        <Link on:click= {() => menuToggle = false} to="compsem-24-web/events/technical" class="p-2  transition-transform duration-75 ease-in-out hover:scale-125 hover:text-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">Technical Events</Link>
-        <Link on:click= {() => menuToggle = false} to="compsem-24-web/events/nontechnical" class="p-2  transition-transform duration-75 ease-in-out hover:scale-125 hover:text-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">Non Technical Events</Link>
-        <a href="/compsem-24-web/#contacts" class="p-2 transition-transform duration-75 ease-in-out hover:scale-125 hover:text-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">Contact Us</a>
+        <Link on:click= {() => menuToggle = false} to="/" class="p-2 transition-transform duration-75 ease-in-out hover:scale-125 rounded-md hover:text-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">Home</Link>
+        <Link on:click= {() => menuToggle = false} to="/events/technical" class="p-2  transition-transform duration-75 ease-in-out hover:scale-125 hover:text-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">Technical Events</Link>
+        <Link on:click= {() => menuToggle = false} to="/events/nontechnical" class="p-2  transition-transform duration-75 ease-in-out hover:scale-125 hover:text-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">Non Technical Events</Link>
+        <a href="/#contacts" class="p-2 transition-transform duration-75 ease-in-out hover:scale-125 hover:text-md hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-300 hover:to-red-400">Contact Us</a>
     </nav>
 </header>
